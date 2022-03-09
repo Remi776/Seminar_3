f93e2ad047f0dcdd95ae90ae011849821a442c33
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-03-09 18:09:48 +0200

    add ex021

---
+++ home_work/ex_19/Program.cs
@@ -0,0 +1,23 @@
+﻿void Palindrome(string number)
+{
+    int i = 0;
+    while (i < number.Length / 2)
+    {
+        if (number[i] = number[number.Length - 1 - i])
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+            Console.WriteLine("it's not a palindrome");
+            break;
+        }
+    Console.WriteLine("it's a palindrome");
+    }
+}
+
+Palindrome(a);
+Console.Write("Enter number: ");
+string a = Console.ReadLine();
+
---
+++ home_work/ex_21/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter the A coordinates");
+int a_x = Convert.ToInt32(Console.ReadLine());
+int a_y = Convert.ToInt32(Console.ReadLine());
+int a_z = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the B coordinates");
+int b_x = Convert.ToInt32(Console.ReadLine());
+int b_y = Convert.ToInt32(Console.ReadLine());
+int b_z = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow((b_x - a_x), 2) + Math.Pow((b_y - a_y), 2) + Math.Pow((b_z - a_z), 2));
+Console.WriteLine("Distance = " + distance);
