29f09ee40911f1363cb3975a8760c21ee6d9f801
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-03-09 18:29:25 +0200

    add ex023

---
+++ home_work/ex_23/Program.cs
@@ -0,0 +1,12 @@
+﻿void Cube(int number)
+{
+    Console.Write(number + " | ");
+    for (int i = 1; i <= number; ++i)
+    {
+        Console.Write(Math.Pow(i, 3) + " | ");
+    }
+}
+
+Console.Write("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Cube(a);
