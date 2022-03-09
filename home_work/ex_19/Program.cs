0de7737220cd08abbc7d157d8a9876acdd0a9675
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-03-09 20:29:27 +0200

    add ex019

--- home_work/ex_19/Program.cs
+++ home_work/ex_19/Program.cs
@@ -1,23 +1,17 @@
-﻿void Palindrome(string number)
+﻿Console.Write("Enter five-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string num_str = Convert.ToString(num);
+string result = "";
+while (num > 0)
 {
-    int i = 0;
-    while (i < number.Length / 2)
-    {
-        if (number[i] = number[number.Length - 1 - i])
-        {
-            i++;
-            continue;
-        }
-        else
-        {
-            Console.WriteLine("it's not a palindrome");
-            break;
-        }
-    Console.WriteLine("it's a palindrome");
-    }
+    result += Convert.ToString(num % 10);
+    num /= 10;
 }
-
-Palindrome(a);
-Console.Write("Enter number: ");
-string a = Console.ReadLine();
-
+if (num_str == result) 
+{
+    Console.WriteLine("It's a palindrome");
+}
+else 
+{
+    Console.WriteLine("It's not a palindrome");
+}
